ac2150b20bca122e1b9b560f078f80d6effbb6ba
OscarDev13 <oscarperezocomatldev@gmail.com>
2024-04-08 23:06:29 -0600

    Mostrando Mapa en pantalla

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,12 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_reader_2/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
    
   const MapaPage({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
+
+    final CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
     // ignore: unused_local_variable
     // final ScanModel scan = ModalRoute.of(context).settings.arguments;
@@ -18,8 +34,12 @@ class MapaPage extends StatelessWidget {
         actionsIconTheme: IconThemeData(color: Colors.white),
         title: const Text('Coordenadas', style: TextStyle( color: Colors.white),),
       ),
-      body: Center(
-         child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
